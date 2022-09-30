c7756ebe7a57e8058fc41fd3cbce5fa90fc59887
axo.dying@gmail.com <90399801+Robin-Rupp@users.noreply.github.com>
2022-10-01 01:40:27 +0200

    Init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { StartpageComponent } from './page/startpage/startpage.component';
+import {PageNotFoundComponent} from "./page/page-not-found/page-not-found.component";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartpageComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
